4156ca2f1ff3cdd15ad9d5763b5c7e023f7618e9
AShaver43 <gofrodude_197@hotmail.com>
2020-08-16 00:43:10 -0700

    organizing

--- src/GUI.java
+++ src/GUI.java
@@ -10,59 +10,61 @@ public class GUI implements ActionListener
     private static JPasswordField passwordText;
     private static JButton button;
     private static JLabel success;
-        public static void main(String[] args) 
+
+    public static void main(String[] args) 
+    {
+
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame();
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        userText = new JTextField(20);
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        passwordText = new JPasswordField();
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        button = new JButton("Login");
+        button.setBounds(10, 80, 80, 25);
+        button.addActionListener(new GUI());
+        panel.add(button);
+
+        success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
+        panel.add(success);
+
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        String user = userText.getText();
+        // New way to get password from JPassword Field
+        String password = new String(passwordText.getPassword());
+        // Old Way
+        // String password = passwordText.getText();
+        String correctPass = "DizzMaster43";
+
+        if (user.equals("Alex") && password.equals(correctPass)) 
         {
-
-            JPanel panel = new JPanel();
-            JFrame frame = new JFrame();
-            frame.setSize(350, 200);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setVisible(true);
-            frame.add(panel);
-
-            panel.setLayout(null);
-
-            userLabel = new JLabel("User");
-            userLabel.setBounds(10, 20, 80, 25);
-            panel.add(userLabel);
-
-            userText = new JTextField(20);
-            userText.setBounds(100, 20, 165, 25);
-            panel.add(userText);
-
-            passwordLabel = new JLabel("Password");
-            passwordLabel.setBounds(10, 50, 80, 25);
-            panel.add(passwordLabel);
-
-            passwordText = new JPasswordField();
-            passwordText.setBounds(100, 50, 165, 25);
-            panel.add(passwordText);
-
-            button = new JButton("Login");
-            button.setBounds(10, 80, 80, 25);
-            button.addActionListener(new GUI());
-            panel.add(button);
-
-            success = new JLabel("");
-            success.setBounds(10, 110, 300, 25);
-            panel.add(success);
-
-            frame.setVisible(true);
+            success.setText("Login Successful!");
         }
-        @Override
-        public void actionPerformed(ActionEvent e) 
-        {
-            String user = userText.getText();
-            // New way to get password from JPassword Field
-            String password = new String(passwordText.getPassword());
-            // Old Way
-            // String password = passwordText.getText();
-            String correctPass = "DizzMaster43";
 
-            if(user.equals("Alex") && password.equals(correctPass)) 
-            {
-                success.setText("Login Successful!");
-            }
-            
-        }
+    }
 }
